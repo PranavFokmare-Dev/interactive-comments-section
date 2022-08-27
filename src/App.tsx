68e833067f3fcbaf77017a8c638d344279af2caa
Pranav <pranav.fokmare.developer@gmail.com>
2022-08-27 15:16:39 +0530

    c2

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import styled from "styled-components";
 
 import { GlobalStyle } from "./GlobalSyles";
-import Comment from "./components/Comment/Comment";
 import { IComment } from "./Models/CommentModel";
-
+import MyComment from "./components/CommentHandler/MyComment";
 
 const AppContainer = styled.div`
   display:flex;
@@ -20,10 +19,12 @@ function App() {
     content: "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
     createdAt: "1 month ago",
     score: 12,
+    replyingTo:-1,
+    isAReply:false,
     user: {
       image: { 
-        png: "./images/avatars/image-amyrobson.png",
-        webp: "./images/avatars/image-amyrobson.webp"
+        png: "images/avatars/image-amyrobson.png",
+        webp: "images/avatars/image-amyrobson.webp"
       },
       username: "amyrobson"
     },
@@ -32,7 +33,7 @@ function App() {
   return (
     <AppContainer>
       <GlobalStyle />
-      <Comment comment={comment}/>
+      <MyComment/>
     </AppContainer>
   );
 }
